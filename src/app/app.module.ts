e6c5cd2850345250ea780ef7534ae82ac1d0de25
Cachin <i1610595@continental.edu.pe>
2020-07-09 18:09:23 -0500

    FRONTENDCASI FINALIZADO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { FooterComponent } from './footer/footer.component';
+import { HabitacionesComponent } from './habitaciones/habitaciones.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ContactenosComponent } from './contactenos/contactenos.component';
+import { ReservaComponent } from './reserva/reserva.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     InicioComponent,
-    FooterComponent
+    FooterComponent,
+    HabitacionesComponent,
+    NosotrosComponent,
+    ContactenosComponent,
+    ReservaComponent
   ],
   imports: [
     BrowserModule,
